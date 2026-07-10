236e77bc2329d2975d0932f6e28cf125f4e3b3e0
kuldeepsingh80067 <kuldeepsingh80067@gmail.com>
2026-07-10 20:50:42 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
+import "./navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">SkillPath</div>
+
+      <div className="logo">
+        <Link to="/">🎓 SkillPath</Link>
+      </div>
 
       <ul className="nav-links">
+
         <li>
           <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/courses">Courses</Link>
         </li>
 
         <li>
@@ -17,7 +26,21 @@ function Navbar() {
         <li>
           <Link to="/contact">Contact</Link>
         </li>
+
       </ul>
+
+      <div className="nav-buttons">
+
+        <Link to="/signin" className="login-btn">
+          Sign In
+        </Link>
+
+        <Link to="/signup" className="signup-btn">
+          Sign Up
+        </Link>
+
+      </div>
+
     </nav>
   );
 }
