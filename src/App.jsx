c28f5d77141f8ee66f98690aa11d441323cf3ef0
kuldeepsingh80067 <kuldeepsingh80067@gmail.com>
2026-07-10 20:46:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
+import Courses from "./pages/Courses";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import NotFoundPage from "./pages/NotFoundPage";
 
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+
+      <Route path="/courses" element={<Courses />} />
+
+      <Route path="/about" element={<AboutPage />} />
+
+      <Route path="/contact" element={<ContactPage />} />
+
+      <Route path="/signin" element={<SignIn />} />
+
+      <Route path="/signup" element={<SignUp />} />
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
